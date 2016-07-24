142db8b9ca8f743f61e28b7c99c5efa306eb741e
lsx <lsx.office.ubuntu@163.com>
2016-07-24 20:00:46 +0800

    不能运行，暂停，回家继续写

---
+++ app/src/main/java/com/fei/chatuiexer/MsgRecyAdapter.java
@@ -0,0 +1,13 @@
+package com.fei.chatuiexer;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by lee on 16-7-24.
+ */
+public class MsgRecyAdapter extends RecyclerView.Adapter<MsgRecyAdapter.MsgViewHolder> {
+
+    public class MsgViewHolder extends RecyclerView.ViewHolder {
+    }
+
+}
